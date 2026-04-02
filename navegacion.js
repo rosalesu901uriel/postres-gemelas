85b0f398f987625892ecf3abd2a5589e9e6a0c27
Uriel Rosales <rosalesu901@gmail.com>
2026-04-02 16:08:14 -0600

    Navegacion celular

--- navegacion.js
+++ navegacion.js
@@ -1,13 +1,29 @@
-// Seleccionar elementos
 const botonMenu = document.querySelector('#boton-menu');
+const btnCerrar = document.querySelector('#btn-cerrar');
 const navegacion = document.querySelector('#navegacion');
+const overlay = document.querySelector('#overlay'); // Seleccionamos el overlay
 
-// Evento para abrir/cerrar menú
+// Función para ABRIR
 botonMenu.addEventListener('click', () => {
-    navegacion.classList.toggle('mostrar-menu');
+    navegacion.classList.add('mostrar-menu');
+    overlay.classList.add('mostrar-overlay');
 });
 
-// Cerrar el menú si hacen clic en un enlace (para móviles)
-navegacion.addEventListener('click', () => {
+// Función para CERRAR (la usaremos en varios clics)
+const cerrarTodo = () => {
     navegacion.classList.remove('mostrar-menu');
+    overlay.classList.remove('mostrar-overlay');
+};
+
+// Cerrar con la X
+btnCerrar.addEventListener('click', cerrarTodo);
+
+// CERRAR SI DAN CLIC AL OVERLAY (Cualquier parte oscura)
+overlay.addEventListener('click', cerrarTodo);
+
+// Cerrar si dan clic a un enlace
+navegacion.addEventListener('click', (e) => {
+    if(e.target.classList.contains('navegacion__enlace')) {
+        cerrarTodo();
+    }
 });
